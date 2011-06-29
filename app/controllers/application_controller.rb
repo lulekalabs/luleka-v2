19a70da56527ff78dc43dfeb77ab7eb70ac3d1c5
Juergen Fesslmeier <juergen@luleka.com>
2011-06-29 18:12:49 -0300

    Adding home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  layout 'slide'
+  
+  protected
+  
+  # should probably be recalled to -> slider?
+  def slide?
+    !!@slide
+  end
+  helper_method :slide?
+  
+  def modal?
+    !!@modal
+  end
+  helper_method :modal?
+  
 end
