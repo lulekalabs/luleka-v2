14cd58b883e50fe8b94c7fec99c2175ded7704bc
Juergen Fesslmeier <juergen@luleka.com>
2011-06-29 15:07:19 -0300

    Adding initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nl::Application.config.secret_token = '562c64bce4bf7016266ad87c8df254b3fea1ce0a7b9ecad8556cd89a8dfce66e7f2047ee7146bc7bff3c16f620278c4d1e46784f7388844cdbda1e6da699965f'
