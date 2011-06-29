a0ea0339a4f78187eebe2e3cfce4cfc4cda85026
Juergen Fesslmeier <juergen@luleka.com>
2011-06-29 18:24:55 -0300

    Adding kases controller/views

---
+++ app/controllers/kases_controller.rb
@@ -0,0 +1,34 @@
+class KasesController < ApplicationController
+  before_filter :load_kase
+  before_filter :settings
+
+  def index
+    @posts = Post.all
+  end
+  
+  def show
+    respond_to do |format|
+      format.js {
+        render :layout => false
+      }
+      format.html {}
+    end
+  end
+  
+  protected
+  
+  def settings
+    @title = "New Luleka"
+    @page_title = "What is your question?"
+    @front_page = false
+    #@active_sidebar = "dashboard-top-area"
+    @active_sidebar = "dashboard-footer-area"
+  end
+
+  def load_kase
+    @kase = Post.find(params[:id])
+    @responses = Post.all
+  rescue ActiveRecord::RecordNotFound
+    @kase = nil
+  end
+end
