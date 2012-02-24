1b47cdebf76aebbf2f82164b08d2ec661682c796
Juergen Fesslmeier <juergen@luleka.com>
2012-02-24 11:24:49 -0300

    Removing existing models

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -27,8 +27,10 @@ $(function() {
   });
 });
 
+/*
 clientSideValidations.validators.local["email_format"] = function(element, options) {
   if (!/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(element.val())) {
     return options.message;
   }
 }
+*/
