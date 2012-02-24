c89a9c8827eade5738fea954da3efe277acbfe3e
Juergen Fesslmeier <juergen@luleka.com>
2012-02-24 11:25:40 -0300

    Removing existing models

--- app/models/context.rb
+++
@@ -1,5 +0,0 @@
-class Context < ActiveRecord::Base
-  belongs_to :kase
-  belongs_to :tier
-  belongs_to :topic
-end
--- app/models/tier.rb
+++
@@ -1,9 +0,0 @@
-class Tier < ActiveRecord::Base
-
-  #--- associations
-  has_many :contexts, :dependent => :destroy
-  has_many :kases,
-    :through => :contexts,
-    :source => :kase,
-  
-end
