1617136dbe6e3042e1c7a1a02fb17175ca3aab4e
Juergen Fesslmeier <juergen@luleka.com>
2012-02-29 11:09:56 -0300

    Preload images

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -10,6 +10,36 @@
 //= require detect_timezone
 //= require_self
 
+(function($) {
+  var imgList = [];
+  $.extend({
+    preload: function(imgArr, option) {
+      var setting = $.extend({
+        init: function(loaded, total) {},
+        loaded: function(img, loaded, total) {},
+        loaded_all: function(loaded, total) {}
+      }, option);
+      var total = imgArr.length;
+      var loaded = 0;
+      
+      setting.init(0, total);
+      for(var i in imgArr) {
+        imgList.push($("<img />")
+          .attr("src", imgArr[i])
+          .load(function() {
+            loaded++;
+            setting.loaded(this, loaded, total);
+            if(loaded == total) {
+              setting.loaded_all(loaded, total);
+            }
+          })
+        );
+      }
+      
+    }
+  });
+})(jQuery);
+
 $(function() {
   /* Hide address bar on iOS devices */ 
   window.scrollTo(0, 1);
@@ -40,6 +70,7 @@ $(function() {
   
   /* Set browser time zone */
   $("form input[name='registration[time_zone]']").val(jstz.determine_timezone().name());
+  
 });
 
 /* For email format client side validations */
