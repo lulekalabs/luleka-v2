ac5d5cf525acb8f55208cc2f29d63b8ea1c6f73a
Juergen Fesslmeier <juergen@luleka.com>
2012-02-29 15:41:05 -0300

    Adding Czech

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,13 +2,14 @@ require "i18n/backend/fallbacks"
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 I18n.fallbacks[:en] = [:en]
-I18n.fallbacks[:de] = [:de, :en]
 I18n.fallbacks[:es] = [:es, :en]
 I18n.fallbacks[:pt] = [:pt, :en]
+I18n.fallbacks[:de] = [:de, :en]
 I18n.fallbacks[:it] = [:it, :en]
 I18n.fallbacks[:fr] = [:fr, :en]
 I18n.fallbacks[:nl] = [:nl, :en]
 I18n.fallbacks[:pl] = [:pl, :en]
+I18n.fallbacks[:cs] = [:cs, :en]
 I18n.fallbacks[:da] = [:da, :en]
 I18n.fallbacks[:sv] = [:sv, :en]
 I18n.fallbacks[:no] = [:no, :en]
