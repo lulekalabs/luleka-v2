97d2915eee01ce7add3a4114ed818c5acceb05bc
Juergen Fesslmeier <juergen@luleka.com>
2012-02-29 09:39:42 -0300

    Added 'has registered' in different languages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < ApplicationController
   
   def create
-    @registration = Registration.instance_for(params[:registration], {
-      :language_code => "#{I18n.locale_language}",
-      :ip_address    => request.remote_ip
-    }, request.location ? request.location.data : {})
+    @registration = Registration.instance_for(params[:registration], 
+      request.location ? request.location.data : {}, {
+        :language_code => "#{I18n.locale_language}",
+        :ip_address    => request.remote_ip
+      })
     @registration.save
     respond_to do |format|
       format.js {}
