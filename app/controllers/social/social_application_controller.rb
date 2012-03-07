3abf2b8f2aaad165d9e9c18f2dd1df3295ce3ec4
Juergen Fesslmeier <juergen@luleka.com>
2012-03-07 17:42:05 -0300

    Updating over page

--- app/controllers/social/social_application_controller.rb
+++ app/controllers/social/social_application_controller.rb
@@ -2,6 +2,7 @@ class Social::SocialApplicationController < ApplicationController
   before_filter :set_p3p_headers
   before_filter :ensure_inside_facebook
   before_filter :find_campaign
+  before_filter :ensure_rules, :except => [:over, :create]
 
   layout "social_application"
 
@@ -27,6 +28,13 @@ class Social::SocialApplicationController < ApplicationController
   def ensure_inside_facebook
     if !inside_facebook? && Rails.env.production?
       redirect_to root_path, :status => :moved_permanently
+      return
+    end
+  end
+
+  def ensure_rules
+    if true # @campaign.over? || @campaign.hasnt_started_yet?
+      redirect_to social_campaign_over_path
       return
     end
   end
